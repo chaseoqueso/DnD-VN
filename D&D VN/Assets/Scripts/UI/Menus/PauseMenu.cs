6a58e3a017f72916221a13b1a2ed92ad0ceba0c8
portaljumperjen <portaljumperjen@gmail.com>
2022-05-03 15:20:47 -0700

    main menu + pause menu + new input system :)

---
+++ D&D VN/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu instance;
+
+    public bool gameIsPaused {get; private set;}
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private GameObject pauseMenuDefaultPanel;
+    [SerializeField] private GameObject settingsMenuPanel;
+
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button settingsBackButton;
+
+    void Awake()
+    {
+        if( instance ){
+            Destroy(gameObject);
+        }
+        else{
+            instance = this;
+        }
+        gameIsPaused = false;
+    }
+
+    public void ResumeGame()
+    {
+        ResetPauseUI();
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void ResetPauseUI()
+    {
+        settingsMenuPanel.SetActive(false);
+        pauseMenuDefaultPanel.SetActive(true);
+
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        continueButton.Select();
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1f;
+        Debug.Log("Returning to Main Menu!");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleSettingsMenu(bool set)
+    {
+        settingsMenuPanel.SetActive(set);
+        pauseMenuDefaultPanel.SetActive(!set);
+
+        if(set){
+            settingsBackButton.Select();
+        }
+        else{
+            continueButton.Select();
+        }
+    }
+}
